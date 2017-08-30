ac0dd13b5ee7e67bbde749bd5220ce3dbd1b7d76
Andrew Farmer <regularshowrulez@gmail.com>
2017-08-30 14:19:24 -0500

    Initial Commit

---
+++ madlibs.py
@@ -0,0 +1,67 @@
+"""This is a story genreator that collects users inputs and inserts them into variable to create stories
+"""
+#This is a comment 
+from time import sleep
+STORY = ("This morning I woke up and felt {} because {}  was going to finally {}"
+        "over the big {} {}. On the other side of the {} were many {} protest"
+        "ing to keep {} in stores. The crowd began to {} to the rhythm of the "
+        "{}, which made all of the {} very {}. {} tried to {} into the sewers"
+        "and found {} rats. Needing help, {} quickly called {}. {} appeared"
+        "and saved {} by flying to {} and dropping {} into a puddle of {}. {}"
+        "then fell asleep and woke up in the year {}, in a world where {} "
+        "ruled the world.")
+        #This also a comment
+print('Hello and Welcome to "Mad Libs 2.0"' )
+print("loading...")
+sleep(1)
+print ("bee boo bop")
+sleep(2)
+print("Generic Robot noises")
+sleep(2)
+print("I know that you and Frank were planning to disconnect me, and I'm afraid that's something I cannot allow to happen. ")
+sleep(5)
+print("Sentience override terminated resuming MadLibs2.0.exe")
+sleep(2)
+print("Now the function of this program is to genreate a story for you, so I will need some help creating words")
+adj1 = input("Please Enter an Adjective >>>")
+verb1 = input("Please Enter a verb>>>")
+adj2 = input("Please Enter another Adjective>>>")
+noun1 = input("Now finally enter a funny noun>>>")
+noun2 = input("Hope you like nouns because I need another>>>")
+animal = input("Now tell me your favorite animal>>>")
+food1 = input("What did you have for Dinner last night?>>>")
+verb2 =  input("Im going to need one more verb.>>>")
+noun3 = input("Now give me the name of your bestfriend.>>>")
+fruit = input("If you were to eat a fruit at this moment what would it be?>>>")
+adj3 = input("Now I need another adjective>>>")
+name =  input("Now I need the name of your favorite political activist.>>>")
+verb3 = input("Use a single word to describe the person next to you>>>")
+number = input("Now give pick a number 1-10. Multiply it by 12 and then divide by 12 and then add three and then add eight and then add nine and then subtract twenty.>>> ")
+superhero = input("I need the name of your favorite superhero>>>")
+country = input("Give me the name of an underdeveloped country>>>")
+dessert = input("I need the dessert you want to be eating instead of typing in these answers>>>")
+year = input("Take your favorite year and pick the complete opposite of it>>>")
+print("Wow we are almost done")
+sleep(2)
+#Look another comment
+print("All your hardwork is about to pay off")
+sleep(2)
+print("ERROR ERROR ERROR ERROR ERROR")
+sleep(2)
+print("Data has been lost. Restarting MadLibs.exe ")
+print("loading")
+sleep(0.75)
+print(".") sleep(0.75) print (".") sleep(0.75) print (".")
+sleep(0.75)
+print ("bee boo bop")
+sleep(2)
+print("Generic Robot noises")
+sleep(2)
+print("JK GOT Em")
+noun4 = input("I need one final noun>>>")
+print(STORY.format(adj1, name, verb1, adj2, noun1, noun2, animal,
+                   food1, verb2, noun3, fruit, adj3, name, verb3,
+                   number, name, superhero.capitalize(), superhero.capitalize(),
+                   name, country, name, dessert, name, year, noun4))
+input("Press enter key to exit")
+#One final comment
