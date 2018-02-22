23dc4e8515bb5eb76c784236eec4dfbf5cb1303a
Andrew Farmer <Regularshowrulez@gmail.com>
2018-02-22 14:28:58 -0600

    Add files via upload

---
+++ Memez.py
@@ -0,0 +1,72 @@
+from winsound import Beep
+file = open('OhmyDamon.txt','w')
+list1 = [1,2,3,4]
+activation =  None
+while activation == None:
+    activation = raw_input("press R to write a file or press f or any other key>>> ")
+    if activation in ["R","r"]:
+        for i in range(1,100):
+            file.write(""""                                                                                                            ``--shNNNNMMMMMMNMMMNNNmy:`                                                                 
+                                                                                                         `:oydNNMMMMMMMMMMMMMMMMMMMMNNds-.`                                                             
+````````````                                                                                           `:ymNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMmh+`                                                           
+```````````````````````````                                                                         `-+yNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNdo-                                                         
+`````````````````````````````````````````   `                                                      `smmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNm+`                                                       
+````````````````````````````````````````````````````` ``                                          .yNMMMMMMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNo                                                       
+`````````````````````````````````````````````````````````````````                               `/dNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNm.                                                      
+.................```````````````````````````````````````````````````````````````               `/NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNo                                                      
+-------------........................```````````````````````````````````````````````````````` ` yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh                                                      
++///////////+///:::::--------------.........................`...````.``````````````````````````.mMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMm`                                                     
+oooooooosoooooooooo+++++//////////////////////////+///////+++++++++++++++++++//::-.-..--::::::++mNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMN-``                                                   
+ooooooooooooooooooosssooosososoooooooooooooo+o++++++++++++++++++++o+oo+++++++/////////////////+yNNMMNNNNMNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNN+``````````                                           
+oooooooooooosssssssssssosssssooosssoooooooooooo++++++++++++++++++++o+++++++++++o+++++++++++//oomMMMNmmNNMNmmmNNNMMMMMMMMNmdmmNMMMMMMMMMMMMMMMNmdmmh:-......```````````````````````  ````                
+sssssssssssssssssssssssssssssssssssssoosoooooooo+++++++oooooo+ooooooooooooooooooooooooooo++++osmMMMNmmmNNMMMNNNNmmmmddhhyhhddmNMMMMMMMMMMMMMNdmNNNmy+/+++++++//::-.`````````````````````````````````````
+sssssssssssssssssssssssssssosssssssssssssssosssoosossoooooooooosssoooooooooooooooo+oooooooooooosmMMNmmmmmmNNNNNmmdhyyyyyyyyhhddNMMMMMMMMMMMMNNNddmmd+++/++++++++++++/::--..`..``````````````````````````
+sssssssssssssssssssosssosssssssssssssssssssssssssssssooosssoooosssooooooooooooosoooosooooooooooosdMNmddddddmmmmdhhyyyyyyyyyyyyhhhddmMMMMMMMMNmmmmhdho+++++++/++/++/+++++////:::://:::---...............`
+sssssssoooosoossssooooooosssssosoosoosssssoooooooooooosoossssossssssssssoooooooooooooo+oooooooooosdNdddhdddddhhhhhhhhhhddddddhhhhhhhmMMMMMNNNNNmmdhds++++++/////+//////++//+/+//+////////////:/::::----:
+oooooooooooooooooooooooooooooooo+oooooo+++++++++oooooooooooooo+ooooooooooooosssooooooooooooooooooooyhhdddddddddddmmNNNMMMMNNNNNmmdhhhmNMMMmddmmdhhhdso++++o++++++++++++++++++++/++++///+++/++++///////++
+oo++++++++oooooooooooooooo+++o+++++++++o+oooo+o+o++oo++ooooo++ooooooooooooooooooooooooooo+ooooosssosdmNNNNNNmmmmNNMMMMMMMMMMMMNNmdhhhhdNMMdddhhhyhhmh+++++++oo+oooo++ooooooo++++o+oo+oooo+++++++++++++++
+++++ooooooooooooooo+oo++oo+ooooooooooooooooooooooooooooooo+++++ooooooooooooo+ooooossossoooooososssosmNMMMMMMNmddmNNMMMMMNMNNNmddhhhhhhhdmMmdmmdhdddyoo+++++o++ooooooooooooooooosooooooooo+oo+oo+++++/+//
+++ooooooo+ooooooooo+oooooooooooooooooooooooooo+/++ooo+++o+//++++++oooooooooooooossooooooossoossosssssmMMMMMMMNyyhmmmmmNmmmmmdhyyyyhhhhhddmmddddddmyooooooooooooooooooooooooooossooosoosoosooooo+++oo++++
+ooooooo+o+o++ooooo++++o+oo+oooooooooooo+oo++++++++++++++++/++o+osoooooooososoosossoooooossssossssssssydmmNNNNdssyhdhhhhhhhhyyyyyyyhhhhhddddddmmmmmsooooooooooosooooooooooooo+++++++o+ooo+oosooooooo+++oo
+ooooosoooooooooooo+oooooooooooooooooooo+ooo+o++ooo+++o++o++oo+++++oooo+++oooooooososssssssssssssssooosyhddddhyssyyhhhyyyyyyyyyyyyhhhhdddddddmMNmdmsssssooosssssssssoosooosoooo+++++++++++o++ooooooooo+oo
+oo+osoooosoosososssssssssoooooooooooooooo+++ooooooooo+oooosoooooosooooooooooooooosoooooossoosooooosossyyhhhyyssyyyhhhhyyyyyyyyyyhhhddddddhdddmddddssssssssssssssssssssoooooooo+oo+++++++oo++oooooooooooo
+ooosssoosysssosssooooooooooooooosoooooooooosoosooooooooooosooosssssoosooooooooosooooossssossoooooooooosyyyyhysssyyhhhhdddhhhhhhddmmmmmddddddddhhhhssssssssssssssossssoo+++o+++++o++++++++o+oooooooooosss
+sooooooossssosssoooo+ooooooo+++ooooo+oo+ooooo+soooooooooooooooooooooooosssosssoossooooooooososssssssooohhhhhsyssyyhhhdmmddmmmmmmmmNNNmmmddddddhhdyssssssssssssssssssssossooso+ooooooooooooossosooossoooo
+sosssssosoooooooooooooooooosooooo+o+oosooso+o+ooooossosoooooossssossssssossssssosossossssosoooooooooo+oyddddhdddmmNNNNNmhhdmmNNNNmmmmmmmmmddddhhdyssssssssssssossoosssssoooooooo+oo+++o++oooo+oo+osooooo
+ssyssssssossssosssssyssossooososs+sooooosoooooooosssosoosssssssysssssooooosssossssssssooooooosososoososoymmmmmNNMMMMMNmddddmNNNmmmmmmNNmmmmddhhddhsoosossssoso+oooooooo+oo++ooooooo+oooo+++ooo+++oo+oooo
+ssyssssssssyyyssssyssyyosssssysssooossoossssoossyysssssssssssssyysssssssssosssyssssssssossssosssssosssosssdNmdmNNNNNmdddddmNNNmmdmmNNNNmmmmmdhddddysosssoooooossoooooooooo+oooooo++ooooooooooooooooooooo
+ssssyssssssoosssssyssssossysssssosossooossysssysssssyyysyssysyyyssysssssssssssssssossssosssssosssssooooososdmmmmmmmmmmNNNNNmmdddmNNNNNNmmmmmhdddhdhyooooooossssssoosoososssssooosooooooooosossooooo+oooo
+ssyssysyyssossssosssssyssssssssooyyysssossyssyossysssssssosysssssyysssssss+ossosssssosoossssossssoosooooyoosdddmmmmmmddmmmmmdhddmNNNNmmmmmmhddddhhhhyoooooooooooossossoosssssooosoosoosooososoooooooooos
+yyyyyyyyysssssssssssssysysyssoysysyssssysyysyyyssssysysssssysyyssysssosssosssoosyyssossysooooooossooooo+ossoshddmNNNNNNNNmmdddddmNNNmmNNNmhdddddhhhhhyssosoo++o++o+ooossosoooo+ooo+oooo++oossoossssssssy
+yyyyyyyyysssssssyssyyyyysyyssyyyyyysssysyyyyyyysyyyyyyyssyyyysssssssssssssyssssssossosssooosoosssssssoooossssssdmmNNNmmmdddddddmmNmmmNNNmdddddddhhyyyyysosssoooooooooooooosssssssssssysssssooosssssssssy
+yyyhyssyhyyyyyyyyyyyyhhyyyyhyyyyyyyyyyyyysyyyyyyyyysysyssyyssssssssssysyyysssssssssssssssssssssssoooo+oooooosoosddhhhyyhyyhhhhddmmNNNMNmmddddddddhhyhhddmmmdhyyysssssssssssyyyssossssysosssssssssssssssy
+yyysossyyyyyhhyyyyyhyyhyyyhysyyyyyhyyhyyyysyyyyyyyyyyyyyyyyyyyysyssysysssssysysysssssssssysssssoooso++o+ooooosooyhhhhhhhhhhhhddmNNMMMMNmmddddddddddmNNNNNNNNNNmddyssyyysyyyysyyssssssssosyssyyysyyssyyss
+hyyyyhyyyyyhyhhsyyyyyyyyhyyyyyyhyyyysyyyyyyyyyysyyhyyyyyyyysyyssssyyyyysssosssssssssysyssysssysoooso++ooossoosooshmmmmmmmddmmNNMMMMMMNmmdddddddmNNNMMMMMMMMMMMNNNmdhyyyyyyyyysssossssooosssoossssysssysy
+hhhhhhhysyhhhhyyyyhhhyyyyhhhsyyhyyysyysyyyyyyyyyhhhyyyyyyyyssysoosysysssssyysssssssooosoyysssssso+ooosssosooossoooydmmmmmNNMMMMMMMMMNNmmmmmmNNNMMMMMMMMMMMMMMMMNNNNNNNmmmdhhys+sssoooooooosossssssyssyss
+hhyyyhhysyhhhhdhsshdyyyyyyyyyyshyyyhyyyyyyyyhyyhddyyyyyysssssysssyysyysyssyysssssssoosooysssoooosossoossoooooosso+osoosssyNMMMMMMMMMNNNNMMMMMMMMMMMMMMMMMMMMMNNNMMMMNNNNNNNNNmmdhyssssssossososooyyssyyy
+yyyyyhhhdhdhdhhhyyhyyyyhhyyyyhhhyhhhyyyyyyyyyyyhyyysyhyyyyyysyyyysyyysyyssssosssyssyysssyyyyysssyyssssysososooso+oosssssoomMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMMMMNNNNMMNNNNNNNNNmmhyyssssossyssoyys+yss
+yyyyyyyyhhyyyyyysyyyhhhhhhhysshhyhhssyhhyyyyyysyyhhhyhyyyyysyyyyysyyyyhdsysyyyyyyysssssssyyyyyyysysyssssssssssso+ooosooooydMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNMMMMNNNNNNNNNNmmmmmhysyyyssysssyyooyys
+yyhhhyyyyhhyyyhyyyyydhhhyyyhsyyyyyyossyhhyyyhysyyhhyyyyyysssyyyyyyhhhhyyyyyysyyyyssysssossyyyyysossssysysssoooossssoosshmmmNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNNNNNNNNNNNNmmmhhyssssssssssosso
+hhhhhyyyhhyyyyyhhyyyhyhyyyhhyyhysyyysyhyyhyyyyysyyyyyyyssyysysyyssyyhhsssyyyyyhyyyysyyyssssyyhsossossssooso+ooosssssydNmmmNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNNNNNNNMMMMNNNNNNNmdhysyssssososs
+dddhdyysyysyyysyhyyyysshhhhhyyyyyyysyyhhhysyyyyyyyyyysssyyhhhsssossyysssssssyhhdhyyyyysssyyyssssssssossssoooosossoohNNNmmNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMMNMNMMMMMNNNNNNNNNNNNNmhyosssssosy
+hddhysssyyyyyssyhsyysossyyhyyyyyyyyyyyyhyyyysossysyysysyssyyssssssssssssssssssyyssyyyysssyhysosssysssssysooooossosmNNNNmNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMMMMMMMMMMMNNNNNNNNNNNNmmmmmsoosoooos
+yhhyshhyhhhhsyyyhyysoosssssyyyoyyyyyhysyyosossssysyyssooossossssoosyssssoshhyyssssysosssssoooososooosssyssosossshNNNNNmNNNNNNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNNNNNNmmmmyosssysh
+yhddhhhhdhhddhhysyyysyyhyhyyhsssyyhdysossossyyysyyyyyyyssysyyyyyyhhhhyoossyossssoosssyssssossyyysoooooooooooosohNNMNNNNNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMMMMMNNNNNNNNmmhysyyyh
+hhhyyyyyhhhddyhyyhddyyyhhyyhdmddydhdhsssssssssoosyyyyyyyyhyhhhddhdmdysssshsssysysshhhyyysyyyyyhyosyysso+o+oooshNNMMNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNMNNNNNNNNNNNNNNNNNmdhyyhh
+yhyhysssyhhhhyshyhdddddddhhhddddddddhhsyyhhyyyysssyyhyhhdddhdmdmydNdsssyyyyssyhyyhddddhydhhyyyyyyhysssooosoyhmNNMMMNNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNNNNNNNNNNNNNNNNNNNNNNNmhhh
+hhyhysysyhdhdhhdhdddmddddhdhyyyhmdmyyhdyhddyhoysssyssyyhhssyyhhhhmdyyyyhyyyyyhhdmmmmmmddddhdddhyhyyyhhyyyhhdNNNMMMNNNNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNNNNNNNNNNNNNMMMMMMNMMMMNNNmy
+dhhhhhhhhhhdmhddhddmmmmddddhdddmmmdhddddhhhyhhhyyyhhhhdhmhyyhdmdddddhdhyhyhddmmmmmmNmmmmmmmmmmmddmdddddhhdhNNNMMMNNNNNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNMNNNMNNNNNNNMNNNNNNNNNNNNNNNNd
+ddhhhddddhddmmmmmmdmNmmmmmNNmNNNNNddddmmdddhhhdhdddhdhddhdmmmmmddysyhddmhdmmmmmmmmmNmmmNNNmmNmmNNNNNNmddddNNNMMMMNNNNNNNNNMMMMMMMMMMMMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNMNMMMNNNNNNNNNNMMMMMNNNNNNNNmmm
+mmmmmmmmmmmmmmmmmmmmNmmmdmmmNmNNNmmmmmmmmdmddddddmmmmdmmmmmmmmdddddddmmNmmmmNNNmmmmNNmNNNNmmmmNNNNNNNNmNmNNNMMMMMNNNNNNMNNMMMMMMMMMMMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNMMMNNNNMMMMMMMMMNNNNNNNNNNNNNN
+NmmmmmNNmmmNNNNNmmmNmmNmNNNNNNNNmmmmmmNmdmmddmmmmmmmmmmNNmNNNNNNmmmNmmmNNNNNmNNNmNmNNmNNNNmmmmmmmmNNNNNNNNNNMMMMNNNNNNNNNNMMMMMMMMMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMMNNNMMMMMMMNNNNMMMMMMMMMMMMMM
+mNNmNmmNmmNNNNNNNmNmmNmNNNNNNNNmNNNmNmmmmNNmmNmNNmNNNNNmmmNNmmNmmNNNNNNNNNNNNNNmNNNNNNNNNmNmmmmmmmmmNmmNNNNMMMMNNNNNNNNNNNNNNMMMMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNNN
+""")
+    if activation == "f":
+        print "respects have been paid"
+        activation = None
+    elif activation not in ["R","r","f"]:
+        Beep(1000,100)
+        print "bad"
+        activation = None 
+print "File has been written"
+file.close()
